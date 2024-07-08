d93056d4ec736f139e9330f860c72b6d643ac94b
Aliu Adedigba <indexcorpng@gmail.com>
2024-07-08 08:09:48 +0100

    Authcontroller-register-done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,22 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function register(Request $request){
-        $field = $request->validation([
+        $field = $request->validate([
             'name' => 'required|max:255',
             'email' => 'required|email|unique:users',
             'password' => 'required|confirmed'
         ]);
         $user = User::create($field);
-        return $user;
+        $token = $user->createToken($request->name);
+
+        return [
+            'user'=> $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
     public function login(Request $request){
-        return 'login';
+       
     }
 
     public function logout(Request $request){
